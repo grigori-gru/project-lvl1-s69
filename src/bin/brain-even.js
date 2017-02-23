f89a753d9254261258417f83789fc6a2d5986cc6
Grigori Grudinin <grgr14@mail.ru>
2017-02-23 18:37:16 +0300

    new logic

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { welcome, hello, yourName, randomInteger } from '..';
+import { welcome, hello, yourName, randomInteger, guess } from '..';
 
 welcome();
 console.log('Answer "yes" if number even otherwise answer "no".');
@@ -9,15 +8,4 @@ hello(name);
 
 const isOdd = num => (num % 2 === 0 ? 'yes' : 'no');
 
-const guessOdd = (acc, item) => {
-  if (acc > 3) return console.log(`Congratulations, ${name}!`);
-  console.log(`Question: ${item}`);
-  const actual = readlineSync.question('Your answer: ');
-  if (isOdd(item) !== actual) {
-    console.log(`${actual} is wrong answer ;(. Correct answer was ${isOdd(item)}.`);
-    return console.log(`Let's try again, ${name}!`);
-  }
-  console.log('Correct!');
-  return guessOdd(acc + 1, randomInteger());
-};
-guessOdd(1, randomInteger());
+guess(1, randomInteger(), isOdd());
--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ export const randomInteger = () => {
   const rand = Math.random() * 100;
   return Math.round(rand);
 };
+
+const guess = (acc, item, func) => {
+  if (acc > 3) return console.log(`Congratulations, ${yourName()}!`);
+  console.log(`Question: ${item}`);
+  const actual = readlineSync.question('Your answer: ');
+  if (func(item) !== actual) {
+    console.log(`${actual} is wrong answer ;(. Correct answer was ${func(item)}.`);
+    return console.log(`Let's try again, ${yourName()}!`);
+  }
+  console.log('Correct!');
+  return guess(acc + 1, randomInteger());
+};
