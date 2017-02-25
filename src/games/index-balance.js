0bf9646fdd361e8ee2dc0cdbd5bc62a03a68ece4
Grigori Grudinin <grgr14@mail.ru>
2017-02-25 14:15:42 +0300

    change sumOfItem

--- src/games/index-balance.js
+++ src/games/index-balance.js
@@ -1,8 +1,10 @@
 import { randomInteger, guess } from '..';
 
 const randNums = randomInteger;
+
+const sumOfItem = x => (x < 10 ? x : Math.floor(x / 10) + sumOfItem(x % 10));
+
 const toResult = (item) => {
-  const sumOfItem = x => (x < 10 ? x : Math.floor(x / 10) + sumOfItem(x % 10));
   const iter = (base, itemLen) => {
     if (base === 0) return '';
     const element = (base % itemLen === 0) ? base / itemLen
