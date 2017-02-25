ca1f1da8e6ab734d50a2ae9ab7186088c1537ac8
Grigori Grudinin <grgr14@mail.ru>
2017-02-25 09:30:02 +0300

    add brain-balance

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ start:
 	npm run babel-node -- src/bin/brain-even.js
 	npm run babel-node -- src/bin/brain-calc.js
 	npm run babel-node -- src/bin/brain-gcd.js
+	npm run babel-node -- src/bin/brain-balance.js
 	npm run publish
 lint:
 	npm run eslint
---
+++ src/games/index-balance.js
@@ -0,0 +1,16 @@
+import { randomInteger, guess } from '..';
+
+const randNums = randomInteger;
+const toResult = (item) => {
+  const sumOfItem = x => (x < 10 ? x : Math.floor(x / 10) + sumOfItem(x % 10));
+  const iter = (base, itemLen) => {
+    if (base === 0) return '';
+    const element = (base % itemLen === 0) ? base / itemLen
+                                           : Math.floor(base / itemLen);
+    return String(element) + iter(base - element, itemLen - 1);
+  };
+  return iter(sumOfItem(item), String(item).length);
+};
+const hello = 'Balance the given number.';
+
+export default () => guess(hello, randNums, toResult);
